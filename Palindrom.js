e2677dab6f0dee4d349fffbf9ea318e5e2102250
mr-pavan-vedpathak <mr.pavanvedpathak@gmail.com>
2022-12-06 22:22:39 +0530

    new branch created by member1

---
+++ Palindrom.js
@@ -0,0 +1,8 @@
+let num=13;
+if(num%1===0 && num%num===0 && num%2!==0 && num%2===1){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+    //this comment for member 2 from member1
+    // "iam the member1 i have write the code for palindrom you have to just create the function for palindrom" 
